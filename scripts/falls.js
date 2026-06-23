d050cd3dddea771158fece8190564d4769c521f6
Matthew Ichinaga <tobinmatthew9@gmail.com>
2026-06-22 21:05:53 -0700

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   .DS_Store
    	modified:   scripts/falls.js
    	modified:   styles/dam-styles.css
    	modified:   styles/falls-styles.css
    	modified:   styles/sense-styles.css
    	modified:   the-falls/the-dam.html

--- scripts/falls.js
+++ scripts/falls.js
@@ -9,6 +9,13 @@ const gameplayText = document.querySelector(".gameplayText");
 const snakeContainer = document.querySelector(".snakeContainer");
 const snakeArray = document.querySelectorAll(".snake")
 
+// A variable just to determine when we finish clicking snakes
+let snakeCounter = 0;
+
+// Our big red button
+const bigRedButtonContainer = document.querySelector(".bigButtonHidden");
+const bigRedButton = document.getElementById("bigRedButton");
+
 // Add a click event to it to fade out text and fade in new instructions and snakes
 continueButton.addEventListener("click", () => {
     continueButton.hidden = true;
@@ -18,3 +25,33 @@ continueButton.addEventListener("click", () => {
 });
 
 // Add random translations to each snake
+for(i of snakeArray){
+    let snakeBoi = i;
+    snakeBoi.addEventListener("click", function snakeMove(){
+        snakeBoi.classList.add("snake-move");
+        snakeCounter++;
+
+        // When all snakes are gone, display new text and button
+        if(snakeCounter >= 4){
+            storyText.textContent = "With the Control Room free of snakes, you see a button on the console.";
+            storyText.classList.remove("fade-out");
+            storyText.classList.add("fade-in");
+            gameplayText.classList.add("fade-out");
+
+            bigRedButtonContainer.classList.add("bigButtonContainer");
+        }
+
+        snakeBoi.removeEventListener("click", snakeMove);
+    });
+}
+
+bigRedButton.addEventListener("click", () => {
+    bigRedButton.hidden = true;
+    let a = document.createElement("a");
+    let linkText = document.createTextNode("Return");
+    a.appendChild(linkText);
+    a.href = "../index.html";
+    storyText.textContent = "The lights flicker on as the dam roars to life. The turbines begin to spin again, sending electricity to far off places. ";
+    storyText.append(a);
+});
+
